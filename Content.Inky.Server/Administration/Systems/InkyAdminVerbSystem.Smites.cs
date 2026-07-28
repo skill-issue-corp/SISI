ce3e1553172dac0fa24c1371845deb3849d51cb3
Shegare <228shadowfox228@gmail.com>
2026-07-29 00:28:26 +0300

    edit admin verbs

--- Content.Inky.Server/Administration/Systems/InkyAdminVerbSystem.Smites.cs
+++ Content.Inky.Server/Administration/Systems/InkyAdminVerbSystem.Smites.cs
@@ -1,41 +1,42 @@
 using Content.Goobstation.Server.Administration.Systems;
 using Content.Inky.Common.Medical;
-using Content.Medical.Common.Body;
 using Content.Shared.Administration;
 using Content.Shared.Body;
 using Content.Shared.Database;
 using Content.Shared.Verbs;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Map.Components;
 using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
-namespace Content.Inky.Server.Administration;
+namespace Content.Inky.Server.Administration.Systems;
 
 /// <summary>
 /// This handles...
 /// </summary>
 public sealed partial class InkyAdminVerbSystem
 {
-    private static readonly ProtoId<OrganCategoryPrototype> Brain = "Brain";
     [Dependency] private GoobAdminVerbSystem _goida = default!;
 
-    /// <inheritdoc/>
-    public void InitializeSmites()
-    {
-        SubscribeLocalEvent<GetVerbsEvent<Verb>>(AddSmiteVerbs);
-    }
+    private static readonly ProtoId<OrganCategoryPrototype> Brain = "Brain";
 
     private void AddSmiteVerbs(GetVerbsEvent<Verb> args)
     {
         if (!_goida.SmitesAllowed(args))
             return;
 
-        Verb autismspectrumdisorder = new()
+        if (!TryComp(args.User, out ActorComponent? actor))
+            return;
+
+        var player = actor.PlayerSession;
+
+        if (!_adminManager.HasAdminFlag(player, AdminFlags.Fun))
+            return;
+
+        args.Verbs.Add(new()
         {
             Text = Loc.GetString("admin-verbs-smite-autism-name".ToLowerInvariant()),
             Category = VerbCategory.Smite,
-            Icon = new SpriteSpecifier.Texture(new("/Textures/_Inky/Interface/Inkymed/alerts.rsi/autism.png")),
+            Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_Inky/Interface/Inkymed/alerts.rsi"), "autism"),
             Act = () =>
             {
                 if (_body.GetOrgan(args.Target, Brain) is { } brain)
@@ -43,7 +44,6 @@ public sealed partial class InkyAdminVerbSystem
             },
             Impact = LogImpact.Extreme,
             Message = Loc.GetString("admin-verbs-smite-autism-desc"),
-        };
-        args.Verbs.Add(autismspectrumdisorder);
+        });
     }
 }
--- Content.Inky.Server/Administration/Systems/InkyAdminVerbSystem.cs
+++ Content.Inky.Server/Administration/Systems/InkyAdminVerbSystem.cs
@@ -1,13 +1,24 @@
-using Content.Server.Administration.Systems;
+using Content.Server.Administration.Managers;
+using Content.Shared.Body;
+using Content.Shared.Verbs;
 
 namespace Content.Inky.Server.Administration.Systems;
 
 public sealed partial class InkyAdminVerbSystem : EntitySystem
 {
+    [Dependency] private IAdminManager _adminManager = default!;
+    [Dependency] private BodySystem _body = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<GetAntagVerbsEvent>(OnGetAntagVerbs);
+        SubscribeLocalEvent<GetVerbsEvent<Verb>>(GetVerbs);
+    }
+
+    private void GetVerbs(GetVerbsEvent<Verb> ev)
+    {
+        AddAdminVerbs(ev);
+        AddSmiteVerbs(ev);
     }
 }
