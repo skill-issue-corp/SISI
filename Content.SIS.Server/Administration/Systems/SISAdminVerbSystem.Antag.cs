3e244d3c3123dd3cec11035363d8580b7a8925dc
Shegare <228shadowfox228@gmail.com>
2026-07-31 00:48:14 +0300

    sis admin verb

---
+++ Content.SIS.Server/Administration/Systems/SISAdminVerbSystem.Antag.cs
@@ -0,0 +1,44 @@
+using Content.Server.Antag;
+using Content.Shared.Administration;
+using Content.Shared.Database;
+using Content.Shared.Mind.Components;
+using Content.Shared.Verbs;
+using Content.Trauma.Server.GameTicking.Rules.Components;
+using Robust.Shared.Player;
+using Robust.Shared.Utility;
+
+namespace Content.SIS.Server.Administration.Systems;
+
+public sealed partial class SISAdminVerbSystem
+{
+    [Dependency] private AntagSelectionSystem _antag = default!;
+
+    private void AddAdminVerbs(GetVerbsEvent<Verb> args)
+    {
+        if (!TryComp<ActorComponent>(args.User, out var actor))
+            return;
+
+        var player = actor.PlayerSession;
+
+        if (!_adminManager.HasAdminFlag(player, AdminFlags.Fun))
+            return;
+
+        if (!HasComp<MindContainerComponent>(args.Target) || !TryComp<ActorComponent>(args.Target, out var targetActor))
+            return;
+
+        var targetPlayer = targetActor.PlayerSession;
+
+        args.Verbs.Add(new()
+        {
+            Text = Loc.GetString("admin-verb-text-make-insurgency"),
+            Category = VerbCategory.Antag,
+            Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/Clothing/Mask/gas.rsi"), "icon"),
+            Act = () =>
+            {
+                _antag.ForceMakeAntag<InsurgencyRuleComponent>(targetPlayer, "Insurgency");
+            },
+            Impact = LogImpact.High,
+            Message = Loc.GetString("admin-verb-text-make-insurgency"),
+        });
+    }
+}
---
+++ Content.SIS.Server/Administration/Systems/SISAdminVerbSystem.cs
@@ -0,0 +1,21 @@
+using Content.Server.Administration.Managers;
+using Content.Shared.Verbs;
+
+namespace Content.SIS.Server.Administration.Systems;
+
+public sealed partial class SISAdminVerbSystem : EntitySystem
+{
+    [Dependency] private IAdminManager _adminManager = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GetVerbsEvent<Verb>>(GetVerbs);
+    }
+
+    private void GetVerbs(GetVerbsEvent<Verb> ev)
+    {
+        AddAdminVerbs(ev);
+    }
+}
