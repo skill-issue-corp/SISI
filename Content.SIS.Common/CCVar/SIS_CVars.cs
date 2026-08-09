c8fa77a94c304d983fdbceb3a4c9dc22d2aee62e
Shegare <228shadowfox228@gmail.com>
2026-08-09 11:54:30 +0300

    init

---
+++ Content.SIS.Common/CCVar/SIS_CVars.cs
@@ -0,0 +1,20 @@
+using Robust.Shared.Configuration;
+
+namespace Content.SIS.Common.CCVar;
+
+[CVarDefs]
+public sealed partial class SIS_CVars
+{
+    #region lowpop
+
+    public static readonly CVarDef<bool> AutoDebug =
+        CVarDef.Create("sis.auto_debug", true, CVar.SERVERONLY);
+
+    public static readonly CVarDef<int> LowPopLimit =
+        CVarDef.Create("sis.low_pop_limit", 15, CVar.SERVERONLY);
+
+    public static readonly CVarDef<int> EngiLowPopLimit =
+        CVarDef.Create("sis.engi_low_pop_limit", 2, CVar.SERVERONLY);
+
+    #endregion
+}
---
+++ Content.SIS.Server/LowPop/AutoDebugSmes.cs
@@ -0,0 +1,85 @@
+using Content.Server.Chat.Systems;
+using Content.Server.Power.SMES;
+using Content.Shared.Power.Components;
+using Content.Shared.Roles.Components;
+using Content.Shared.Roles.Jobs;
+using Content.Shared.SSDIndicator;
+using Content.SIS.Common.CCVar;
+using Robust.Server.Player;
+using Robust.Shared.Configuration;
+
+namespace Content.SIS.Server.LowPop;
+
+public sealed partial class AutoDebugSmes : EntitySystem
+{
+    [Dependency] private IConfigurationManager _cfg = default!;
+    [Dependency] private IPlayerManager _playerMan = default!;
+    [Dependency] private SharedJobSystem _jobSystem = default!;
+    [Dependency] private ChatSystem _chat = default!;
+
+    private readonly LocId _engiDep = "department-Engineering";
+
+    private const float DelaySeconds = 1f * 60f;
+    private float _timer = 0;
+
+    private bool _handled = false;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        _timer += frameTime;
+        if (_timer < DelaySeconds)
+            return;
+        _timer = 0;
+
+        if (!_cfg.GetCVar(SIS_CVars.AutoDebug) || _handled)
+            return;
+
+        var lowPopLimit = _cfg.GetCVar(SIS_CVars.LowPopLimit);
+        if (_playerMan.PlayerCount > lowPopLimit)
+            return;
+
+        var engiLowPopLimit = _cfg.GetCVar(SIS_CVars.EngiLowPopLimit);
+        if (CheckEngiCondition(engiLowPopLimit))
+            return;
+
+        var smesQuery = EntityQueryEnumerator<SmesComponent>();
+        while (smesQuery.MoveNext(out var uid, out _))
+        {
+            var recharger = EnsureComp<BatterySelfRechargerComponent>(uid);
+            var battery = EnsureComp<BatteryComponent>(uid);
+
+            recharger.AutoRechargeRate = battery.MaxCharge;
+            recharger.AutoRechargePauseTime = TimeSpan.Zero;
+            Dirty(uid, recharger);
+        }
+
+        _chat.DispatchGlobalAnnouncement(Loc.GetString("auto-debug-smes-announcement"), colorOverride: Color.Yellow);
+        _handled = true;
+    }
+
+    private bool CheckEngiCondition(int engiLowPopLimit)
+    {
+        var engiCount = 0;
+
+        var playersQuery = EntityQueryEnumerator<SSDIndicatorComponent, MindRoleComponent>();
+        while (playersQuery.MoveNext(out _, out var ssdIndicatorComp, out var mindRoleComp))
+        {
+            if (ssdIndicatorComp.IsSSD)
+                continue;
+
+            if (mindRoleComp.JobPrototype is not {} job)
+                continue;
+
+            _jobSystem.TryGetDepartment(job.Id, out var department);
+
+            if (_engiDep == department?.Name)
+                engiCount++;
+
+            if (engiCount >= engiLowPopLimit)
+                return true;
+        }
+        return false;
+    }
+}
