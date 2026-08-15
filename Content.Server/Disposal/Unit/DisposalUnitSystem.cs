9db257fb95534ea4f8c7fa4943bde23dfb716a57
Shegare <228shadowfox228@gmail.com>
2026-08-15 13:58:53 +0300

    1

--- Content.Server/Disposal/Unit/DisposalUnitSystem.cs
+++ Content.Server/Disposal/Unit/DisposalUnitSystem.cs
@@ -1,14 +1,7 @@
-// SPDX-FileCopyrightText: 2025 Ilya246
-// SPDX-FileCopyrightText: 2025 ark1368
-//
-// SPDX-License-Identifier: MPL-2.0
-
 using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
-using Content.Shared.Destructible;
 using Content.Shared.Disposal.Components;
 using Content.Shared.Disposal.Unit;
-using Content.Shared.Explosion;
 
 namespace Content.Server.Disposal.Unit;
 
@@ -21,25 +14,14 @@ public sealed partial class DisposalUnitSystem : SharedDisposalUnitSystem
     /// <inheritdoc/>
     protected override void IntakeAir(Entity<DisposalUnitComponent> ent, TransformComponent xform)
     {
-        base.Initialize();
+        var air = ent.Comp.Air;
+        var indices = _xform.GetGridTilePositionOrDefault((ent, xform));
 
-        SubscribeLocalEvent<DisposalUnitComponent, DestructionEventArgs>(OnDestruction);
-        SubscribeLocalEvent<DisposalUnitComponent, EntityTerminatingEvent>(OnTerminating);
-        SubscribeLocalEvent<DisposalUnitComponent, BeforeExplodeEvent>(OnExploded);
-    }
+        if (_atmos.GetTileMixture(xform.GridUid, xform.MapUid, indices, true) is { Temperature: > 0f } environment)
+        {
+            var transferMoles = 0.1f * (0.25f * Atmospherics.OneAtmosphere * 1.01f - air.Pressure) * air.Volume / (environment.Temperature * Atmospherics.R);
 
-    private void OnDestruction(EntityUid uid, DisposalUnitComponent component, DestructionEventArgs args)
-    {
-        // TryEjectContents(uid, component); // TODO-SIS: Бля
-    }
-
-    private void OnTerminating(Entity<DisposalUnitComponent> ent, ref EntityTerminatingEvent args)
-    {
-        // TryEjectContents(ent, ent.Comp); // TODO-SIS: Бля
-    }
-
-    private void OnExploded(Entity<DisposalUnitComponent> ent, ref BeforeExplodeEvent args)
-    {
-        // args.Contents.AddRange(ent.Comp.Container.ContainedEntities); // TODO-SIS: Бля
+            ent.Comp.Air = environment.Remove(transferMoles);
+        }
     }
 }
