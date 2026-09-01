6d44937c746d9633d3372f5a0dd3b23b4f0773ae
Lotteryok <iotteryok@gmail.com>
2026-09-01 23:05:17 +0300

    переписал под сервер

---
+++ Content.SIS.Server/Animal/ScaleMothSystem.cs
@@ -0,0 +1,29 @@
+﻿using Content.Shared.Sprite;
+using Content.SIS.Common.Animal;
+
+namespace Content.SIS.Server.Animal;
+
+public sealed partial class ScaleMothSystem : EntitySystem
+{
+    [Dependency] private SharedScaleVisualsSystem _scale = default!;
+    public override void Initialize()
+
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ScaleMothComponent, EntEatIt>(MothEaten);
+
+    }
+
+    public void MothEaten(EntityUid uid, ScaleMothComponent component, EntEatIt eat)
+    {
+        component.MothEatIt = true;
+
+        if (component.MothEatIt)
+        {
+            var currentScale = _scale.GetSpriteScale(uid);
+            _scale.SetSpriteScale(uid, currentScale + component.ScaleMoth);
+        }
+        component.MothEatIt = false;
+    }
+}
